9003d7d1394df1b93766ccee46280157fa81ad84
Tyler Parker <tparker42d2@gmail.com>
2021-08-05 07:56:49 -0600

    resolved function that wasn't a function error, still working to filter results.

--- src/components/HeaderContent.jsx
+++ src/components/HeaderContent.jsx
@@ -9,10 +9,12 @@ import {
 } from "@chakra-ui/react"; 
 import { ColorModeSwitcher } from '../ColorModeSwitcher';
 import { IncidentContextConsumer } from '../context/IncidentContext';
+import { usStates } from './States';
 
 export function HeaderContent() {
-    const stateCodes = [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY' ];
-    // const incident = useContext(IncidentContextConsumer)
+    // const stateCodes = [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY' ];
+    // const states = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+
     return (
             <>
                 <Flex align="center" mr={5}>  
@@ -22,15 +24,15 @@ export function HeaderContent() {
                     </Flex>  
         
                     <Flex justifyContent='flex-end' alignItems='space-around'>
-                        {/* <IncidentContextConsumer> */}
-
+                        <IncidentContextConsumer>
+                            { context => 
                                 <Select placeholder="AL" size='lg' mr={5}>
-                                    {stateCodes.map(state => <option value={state}>
-                                        {state}
+                                    {usStates.map(state => <option key={state.abbreviation} onClick={() => context.getStateData(state.name)} >
+                                        {state.abbreviation}
                                     </option>)}
                                 </Select>
-   
-                        {/* </IncidentContextConsumer> */}
+                            }
+                        </IncidentContextConsumer>
 
                         <Link href='https://github.com/tyler-parker/police-brutality-project' isExternal>
                             <IconButton icon={<VscGithub />} isRound ></IconButton>
--- src/context/IncidentContext.js
+++ src/context/IncidentContext.js
@@ -7,6 +7,15 @@ class IncidentContext extends React.Component {
         incidentsArr: []
     }
 
+    getStateData = (stateName) => {
+        axios.get(`https://api.846policebrutality.com/api/incidents?filter[state]=${stateName}`)
+            .then(res => {
+                this.setState({incidentsArr: [...res.data.data]})
+                console.log("state data", res.data.data)
+            })
+            .catch(err => console.log(err))
+    }
+
     componentDidMount(){
         axios.get("https://api.846policebrutality.com/api/incidents?page[limit]=10")
             .then(res => res)
@@ -16,18 +25,9 @@ class IncidentContext extends React.Component {
             .catch(err => console.log(err)) 
     }
 
-    getStateData = (stateCode) => {
-        axios.get(`https://api.vschool.io/tyler-parker/thing/${stateCode}`)
-            .then(res => {
-                console.log("state data", res.data.data)
-                this.setState({incidentsArr: [...res.data.data]})
-            })
-            .catch(err => console.log(err))
-    }
-
     render(){
         return(
-            <Provider value={{incidentsArr: this.state.incidentsArr}}>
+            <Provider value={{incidentsArr: this.state.incidentsArr, getStateData: this.getStateData}}>
                 {this.props.children}
             </Provider>
         )
